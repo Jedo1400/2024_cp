6ba1d372c087d7a85d87a92370bf241da6d56c59
Jedo1400 <163664998+Jedo1400@users.noreply.github.com>
2024-05-08 11:42:59 +0900

    문제4.cpp 업데이트

--- 05.08/문제4.cpp
+++ 05.08/문제4.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     for (short i = 1; i <= 10; ++i) {
         bool sex = rand() % 2;
-        short height = rand() % 220 + 1, weight = rand() % 120 + 1;
+        short height = rand() % 121 + 100, weight = rand() % 86 + 35;
     
         cout << i << ']' << (sex ? "남자" : "여자") << '-' << height << '-' << weight << endl;
     }
