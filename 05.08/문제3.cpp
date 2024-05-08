236d1c27fbe4d2118977f0ff20e5ece6077e0bb2
Jedo1400 <163664998+Jedo1400@users.noreply.github.com>
2024-05-08 10:59:39 +0900

    Create 문제3.cpp

---
+++ 05.08/문제3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    short answer;
+    cout << "가위 = 0, 바위 = 1, 보 = 2" << endl;
+    cin >> answer;
+    const short result = rand() % 3;
+    if (answer == result)
+        cout << "비겼습니다";
+    else if (answer + result != 2)
+        cout << (answer > result ? "이겼습니다" : "졌습니다");
+    else
+        cout << (answer < result ? "이겼습니다" : "졌습니다");
+    cout << endl << "내가 낸 패 : " << answer << ", 컴퓨터가 낸 패 : " << result;
+}
